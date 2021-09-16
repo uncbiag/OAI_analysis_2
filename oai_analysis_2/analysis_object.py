c52d321c16ceb48108b5b8a2c03e04f35b182bf9
Hastings Greer <hastings.greer@gmail.com>
2021-09-16 18:47:57 -0400

    Cpu support- may be rolled back
    Former-commit-id: e1f8759cc27b4852c0dd4c43984d63ca7040176c

--- oai_analysis_2/analysis_object.py
+++ oai_analysis_2/analysis_object.py
@@ -1,5 +1,5 @@
 import itk
-
+import torch
 from oai_analysis_2 import utils
 import oai_analysis_2.segmentation.segmenter
 
@@ -7,13 +7,17 @@ import os
 
 class AnalysisObject:
     def __init__(self):
+        if "cuda" in torch.testing.get_all_device_types():
+            self.device = "cuda"
+        else:
+            self.device = "cpu"
         ## Initialize segmenter
 
         segmenter_config = dict(
             ckpoint_path=os.path.join(utils.get_data_dir(), "segmentation_model.pth.tar"),
             training_config_file=os.path.join(utils.get_data_dir()
             , "segmentation_train_config.pth.tar"),
-            device="cuda",
+            device=self.device,
             batch_size=4,
             overlap_size=(16, 16, 8),
             output_prob=True,
