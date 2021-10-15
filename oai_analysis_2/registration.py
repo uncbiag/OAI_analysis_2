c40006a3f7ce51e4d23098e0b02e6827f281e8dc
Hastings Greer <hastings@hgreer.com>
2021-10-15 16:30:14 -0400

    registration progress

---
+++ oai_analysis_2/registration.py
@@ -0,0 +1,12 @@
+import easyreg
+import mermaid
+
+
+
+class AVSM_Registration:
+    def __init__(self, ckpoint_path):
+        pass
+
+    def register(self, fixed_image, moving_image):
+        return 0
+
