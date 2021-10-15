99f91ae7b65fc86f85126142c1879355fc9b2eb4
Hastings Greer <hastings@hgreer.com>
2021-10-15 17:06:06 -0400

    config for avsm
    Former-commit-id: 3eaf147e103b012b2173e0eb69e5ff02f97af9df

--- oai_analysis_2/registration.py
+++ oai_analysis_2/registration.py
@@ -4,7 +4,8 @@ import mermaid
 
 
 class AVSM_Registration:
-    def __init__(self, ckpoint_path):
+    def __init__(self, ckpoint_path, config_path):
+        
         pass
 
     def register(self, fixed_image, moving_image):
