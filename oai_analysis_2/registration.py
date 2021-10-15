4a04557c1429918d50d01e9aaa80bebc5b7cdd29
Hastings Greer <hastings@hgreer.com>
2021-10-15 19:56:28 -0400

    half way there
    Former-commit-id: ef4b7c9092163f17674e74f14ae75af5c3502e24

--- oai_analysis_2/registration.py
+++ oai_analysis_2/registration.py
@@ -1,13 +1,31 @@
 import easyreg
+import demo.demo_for_easyreg_eval as demo
 import mermaid
 
+import icon_registration.pretrained_models
+
+import torch
 
 
 class AVSM_Registration:
     def __init__(self, ckpoint_path, config_path):
-        
-        pass
+        self.ckpoint_path = ckpoint_path
+        self.config_path = config_path 
+
 
     def register(self, fixed_image, moving_image):
-        return 0
+
+
+        #write_images
+
+        demo.do_registration_eval(args, [fixed_image_path, moving_image_path, none, none])
+
+class ICON_Registration:
+    def __init__(self):
+        self.register_object = icon_registration.pretrained_models.OAI_knees_registration_model(pretrained=True)
+
+    def register(self, fixed_image, moving_image):
+        self.register_object
+
+
 
