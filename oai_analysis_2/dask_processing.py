35c231b3339dd1c29974153594c3eaa805ba6c8f
Pranjal Sahu <pranjalsahu5@gmail.com>
2022-08-22 00:27:43 -0400

    Updated code that reads data from zarr files

--- oai_analysis_2/dask_processing.py
+++ oai_analysis_2/dask_processing.py
@@ -2,190 +2,188 @@
 
 import numpy as np
 import os
-#os.environ["CUDA_VISIBLE_DEVICES"]=""
-
-
 import dask
 from dask import delayed, compute, visualize
 from dask.distributed import Client, progress, LocalCluster
 
 
+def image_normalize(image, window_min_perc, window_max_perc, output_min, output_max):
+    import itk
+    from itk import IntensityWindowingImageFilter as IntensityWindowingImageFilter
+
+    window_rescale = IntensityWindowingImageFilter[type(image), type(image)].New()
+    image_array = itk.GetArrayFromImage(image)
+
+    window_min = np.percentile(image_array, window_min_perc)
+    window_max = np.percentile(image_array, window_max_perc)
+
+    window_rescale.SetInput(image)
+    window_rescale.SetOutputMaximum(output_max)
+    window_rescale.SetOutputMinimum(output_min)
+    window_rescale.SetWindowMaximum(window_max)
+    window_rescale.SetWindowMinimum(window_min)
+    window_rescale.Update()
+    return window_rescale.GetOutput()
+
+
+def readimage(image_path):
+    import itk
+    from itk import CastImageFilter as CastImageFilter
+    import xarray as xr
+
+    xr_dataset = xr.open_zarr(image_path)
+    xr_array = xr_dataset.get("image")
+    itk_image = itk.image_from_xarray(xr_array)
+
+    cast_filter_type = CastImageFilter[type(itk_image), itk.Image[itk.F, 3]].New()
+    cast_filter_type.SetInPlace(False)
+    cast_filter_type.SetInput(itk_image)
+    cast_filter_type.Update()
+    itk_image = cast_filter_type.GetOutput()
+    return itk_image
+
 
 @delayed(nout=3)
 def register_images_delayed(image_A, image_B):
-    import boto3
     import itk
+    from itk import CastImageFilter as CastImageFilter
     import icon_registration
     import icon_registration.itk_wrapper as itk_wrapper
     import icon_registration.pretrained_models as pretrained_models
-    from os.path import exists
     import torch
+    import xarray as xr
     import time
-    torch.set_num_threads(8)
-    
-    # Create temporary file names to download
-    image_A_filename = str(int(time.time()))+'_image_preprocessed.nii.gz'
-    image_B_filename = str(int(time.time()))+'_atlas_image.nii.gz'
-    
-    s3          = boto3.resource("s3")
-    bucket_name = 'oaisample1'
-    bucket      = s3.Bucket(bucket_name)
+    import numpy as np
+    import gc
 
-    s3.Bucket(bucket_name).download_file(image_A, image_A_filename)
-    s3.Bucket(bucket_name).download_file(image_B, image_B_filename)
-    
-    image_A = itk.imread(image_A_filename, itk.D)
-    image_B = itk.imread(image_B_filename, itk.D)
+    image_A = readimage(image_A)
+    image_B = readimage(image_B)
+
+    cast_filter_type = CastImageFilter[type(image_A), itk.Image[itk.D, 3]].New()
+    cast_filter_type.SetInPlace(False)
+    cast_filter_type.SetInput(image_A)
+    cast_filter_type.Update()
+    image_A = cast_filter_type.GetOutput()
+
+    cast_filter_type = CastImageFilter[type(image_B), itk.Image[itk.D, 3]].New()
+    cast_filter_type.SetInPlace(False)
+    cast_filter_type.SetInput(image_B)
+    cast_filter_type.Update()
+    image_B = cast_filter_type.GetOutput()
+
+    image_A = image_normalize(image_A, 0.1, 99.9, 0, 1)
 
     model = pretrained_models.OAI_knees_gradICON_model()
     if torch.cuda.is_available():
         model.cuda()
+        torch.cuda.empty_cache()
     else:
-        model.to('cpu')
+        model.to("cpu")
 
     # Register the images
     phi_AB, phi_BA = itk_wrapper.register_pair(model, image_A, image_B)
-    return itk.dict_from_transform(phi_AB), itk.dict_from_image(image_A), itk.dict_from_image(image_B)
+
+    del model
+    gc.collect()
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
+
+    return phi_AB, image_A, image_B
 
 
 @delayed
-def deform_probmap_delayed(phi_AB, image_A, image_B, prob, image_type='FC'):
+def deform_probmap_delayed(phi_AB, image_A, image_B, prob, image_type="FC"):
     import itk
+    from itk import LinearInterpolateImageFunction
 
-    phi_AB1 = itk.transform_from_dict(phi_AB)
-
-    def set_parameters(phi_AB, phi_AB1):
-        for i in range(len(phi_AB) - 1):
-            transform1 = phi_AB1.GetNthTransform(i)
-
-            fp = phi_AB[i + 1]['fixedParameters']
-            o1 = transform1.GetFixedParameters()
-            o1.SetSize(fp.shape[0])
-            for j, v in enumerate(fp):
-                o1.SetElement(j, v)
-            transform1.SetFixedParameters(o1)
-
-            p = phi_AB[i + 1]['parameters']
-            o2 = transform1.GetParameters()
-            o2.SetSize(p.shape[0])
-            for j, v in enumerate(p):
-                o2.SetElement(j, v)
-            transform1.SetParameters(o2)
-
-    set_parameters(phi_AB, phi_AB1)
-    image_A = itk.image_from_dict(image_A)
-    image_B = itk.image_from_dict(image_B)
-
-    interpolator = itk.LinearInterpolateImageFunction.New(image_A)
-
-    prob = itk.image_from_dict(prob)
-
+    interpolator = LinearInterpolateImageFunction.New(image_A)
     warped_image = itk.resample_image_filter(
         prob,
-        transform=phi_AB1,
+        transform=phi_AB,
         interpolator=interpolator,
         size=itk.size(image_B),
         output_spacing=itk.spacing(image_B),
         output_direction=image_B.GetDirection(),
-        output_origin=image_B.GetOrigin())
+        output_origin=image_B.GetOrigin(),
+    )
 
-    output_dict = itk.dict_from_image(warped_image)
-    return output_dict
+    return warped_image
+
 
 @delayed(nout=1)
 def get_thickness(warped_image, mesh_type):
     import itk
-    import vtk
     import numpy as np
     from oai_analysis_2 import mesh_processing as mp
 
-    def get_itk_mesh(vtk_mesh):
-        Dimension = 3
-        PixelType = itk.D
-        
-        # Get points array from VTK mesh
-        points = vtk_mesh.GetPoints().GetData()
-        points_numpy = np.array(points).flatten()#.astype('float32')
-            
-        polys = vtk_mesh.GetPolys().GetData()
-        polys_numpy = np.array(polys).flatten()
-
-        # Triangle Mesh
-        vtk_cells_count = vtk_mesh.GetNumberOfPolys()
-        polys_numpy = np.reshape(polys_numpy, [vtk_cells_count, Dimension+1])
-
-        # Extracting only the points by removing first column that denotes the VTK cell type
-        polys_numpy = polys_numpy[:, 1:]
-        polys_numpy = polys_numpy.flatten().astype(np.uint64)
-
-        # Get point data from VTK mesh to insert in ITK Mesh
-        point_data_numpy = np.array(vtk_mesh.GetPointData().GetScalars())#.astype('float64')
-        
-        # Get cell data from VTK mesh to insert in ITK Mesh
-        cell_data_numpy = np.array(vtk_mesh.GetCellData().GetScalars())#.astype('float64')
-        
-        MeshType = itk.Mesh[PixelType, Dimension]
-        itk_mesh = MeshType.New()
-        
-        itk_mesh.SetPoints(itk.vector_container_from_array(points_numpy))
-        itk_mesh.SetCellsArray(itk.vector_container_from_array(polys_numpy), itk.CommonEnums.CellGeometry_TRIANGLE_CELL)
-        itk_mesh.SetPointData(itk.vector_container_from_array(point_data_numpy))
-        itk_mesh.SetCellData(itk.vector_container_from_array(cell_data_numpy))    
-        return itk_mesh
-
     warped_image = itk.image_from_dict(warped_image)
-    distance_inner, distance_outer = mp.get_thickness_mesh(warped_image, mesh_type=mesh_type)
-    distance_inner_itk = get_itk_mesh(distance_inner)
-    distance_inner_itk_dict = itk.dict_from_mesh(distance_inner_itk)
-
-    return distance_inner_itk_dict
+    distance_inner, _ = mp.get_thickness_mesh(warped_image, mesh_type=mesh_type)
+    distance_inner_itk = mp.get_itk_mesh(distance_inner)
+    return distance_inner_itk
 
 
 @delayed(nout=2)
 def segment_method(image_A):
+    import itk
+    from itk import IntensityWindowingImageFilter as IntensityWindowingImageFilter
     import oai_analysis_2
     import torch
     import os
     from os.path import exists
     from oai_analysis_2 import utils
     from oai_analysis_2.segmentation import segmenter
-    import boto3
-    import itk
-    import time
+    import numpy as np
+    import urllib.request
+    import gc
 
     if torch.cuda.is_available():
         device = "cuda"
+        torch.cuda.empty_cache()
     else:
         print("WARNING: CUDA NOT AVAILABLE, FALLING BACK TO CPU")
         device = "cpu"
 
+    if not exists("segmentation_model.pth.tar"):
+        print("Downloading segmentation model")
+        urllib.request.urlretrieve(
+            "https://github.com/uncbiag/OAI_analysis_2/blob/master/data/segmentation_model.pth.tar?raw=true",
+            "segmentation_model.pth.tar",
+        )
+
+    if not exists("segmentation_train_config.pth.tar"):
+        urllib.request.urlretrieve(
+            "https://github.com/uncbiag/OAI_analysis_2/blob/master/data/segmentation_train_config.pth.tar?raw=true",
+            "segmentation_train_config.pth.tar",
+        )
+
     # Initialize segmenter
     segmenter_config = dict(
-        ckpoint_path=os.path.join(utils.get_data_dir(),
-                                  "segmentation_model.pth.tar"),
-        training_config_file=os.path.join(utils.get_data_dir(),
-                                          "segmentation_train_config.pth.tar"),
+        ckpoint_path="segmentation_model.pth.tar",
+        training_config_file="segmentation_train_config.pth.tar",
         device=device,
-        batch_size=4,
+        batch_size=2,
         overlap_size=(16, 16, 8),
         output_prob=True,
         output_itk=True,
     )
 
     segmenter = oai_analysis_2.segmentation.segmenter.Segmenter3DInPatchClassWise(
-        mode="pred", config=segmenter_config)
-
-    # Download image
-    image_A_filename = str(int(time.time()))+'_image_preprocessed.nii.gz'
-    s3 = boto3.resource("s3")
-    bucket_name = 'oaisample1'
-    bucket = s3.Bucket(bucket_name)
-    s3.Bucket(bucket_name).download_file(image_A, image_A_filename)
+        mode="pred", config=segmenter_config
+    )
 
     # Segment downloaded image
-    test_volume = itk.imread(image_A_filename, itk.F)
-    FC_probmap, TC_probmap = segmenter.segment(test_volume,
-                                               if_output_prob_map=True,
-                                               if_output_itk=True)
+    test_volume = readimage(image_A)
 
-    return itk.dict_from_image(FC_probmap), itk.dict_from_image(TC_probmap)
+    test_volume = image_normalize(test_volume, 0.1, 99.9, 0, 1)
+
+    FC_probmap, TC_probmap = segmenter.segment(
+        test_volume, if_output_prob_map=True, if_output_itk=True
+    )
+
+    del segmenter
+    gc.collect()
+
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
+
+    return FC_probmap, TC_probmap
