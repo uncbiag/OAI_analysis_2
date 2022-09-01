58254c8ea12d0ea2c8799d40850b91339f618d91
Pranjal Sahu <pranjalsahu5@gmail.com>
2022-09-01 10:46:05 -0400

    Adding test for thickness computation
    Former-commit-id: b4fb08cf337e15b3bc4d0eecf6e0b77d3f7df491

--- oai_analysis_2/mesh_processing.py
+++ oai_analysis_2/mesh_processing.py
@@ -214,7 +214,7 @@ def split_tibial_cartilage_surface(mesh, mesh_normals, mesh_centroids):
         (mesh_centroids_normalized * 1, mesh_normals * 10), axis=1
     )
 
-    est = KMeans(n_clusters=2, algorithm="full")
+    est = KMeans(n_clusters=2, algorithm="full", random_state=5)
     labels = est.fit(features).labels_
 
     # transfer 0/1 labels to -1/1 labels
@@ -238,7 +238,7 @@ def cluster_and_segment(mesh_centroids_normalized, face_normal_value, dot_output
     features = np.concatenate(
         (mesh_centroids_normalized * 1, face_normal_value, dot_output), axis=1
     )
-    est1 = KMeans(n_clusters=2, algorithm="full", n_init=5)
+    est1 = KMeans(n_clusters=2, algorithm="full", n_init=5, random_state=5)
     labels_upper = est1.fit(features).labels_
     labels_upper = labels_upper * 2 - 1
 
