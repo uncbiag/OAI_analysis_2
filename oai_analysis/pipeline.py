befc0a2062d278c042834803637c277c6237ebea
Dženan Zukić <dzenan.zukic@kitware.com>
2024-09-06 14:30:47 -0400

    ENH: Convert VTK mesh into ITK format and vice versa via files
    These intermediate files can also be used for inspection later.

--- oai_analysis/pipeline.py
+++ oai_analysis/pipeline.py
@@ -6,25 +6,37 @@ import itk
 import matplotlib.pyplot as plt
 import icon_registration.itk_wrapper as itk_wrapper
 import icon_registration.pretrained_models as pretrained_models
+import vtk
 
 from analysis_object import AnalysisObject
 import mesh_processing as mp
 
 
-def transform_mesh(mesh, transform):
+def transform_mesh(mesh, transform, filename_prefix):
     """
     Transform the input mesh using the provided transform.
 
     :param mesh: input mesh
     :param transform: The modelling transform to use (the inverse of image resampling transform)
+    :param filename_prefix: prefix (including path) for the intermediate file names
     :return: transformed mesh
     """
-    ttype = itk.TransformMeshFilter[itk.Mesh[itk.SS, 3], itk.Mesh[itk.SS, 3], itk.Transform[itk.D, 3, 3]]
-    filter = ttype()
-    filter.SetInput(mesh)
-    filter.SetTransform(transform)
-    filter.Update()
-    transformed_mesh = filter.GetOutput()
+    writer = vtk.vtkPolyDataWriter()
+    writer.SetFileName(filename_prefix + "_original.vtk")
+    writer.SetInputData(mesh)
+    writer.SetFileVersion(42)  # ITK does not support newer version (5.1)
+    writer.SetFileTypeToBinary()  # reading and writing binary files is faster
+    writer.Write()
+
+    itk_mesh = itk.meshread(filename_prefix + "_original.vtk")
+    t_mesh = itk.transform_mesh_filter(itk_mesh, transform=transform)
+    # itk.meshwrite(t_mesh, filename_prefix + "_transformed.vtk", binary=True)  # does not work in 5.4 and earlier
+    itk.meshwrite(t_mesh, filename_prefix + "_transformed.vtk", compression=True)
+
+    reader = vtk.vtkPolyDataReader()
+    reader.SetFileName(filename_prefix + "_transformed.vtk")
+    reader.Update()
+    transformed_mesh = reader.GetOutput()
     return transformed_mesh
 
 
@@ -36,6 +48,7 @@ def analysis_pipeline(input_path, output_path):
     :param output_path: path to the desired directory for outputs.
     """
     in_image = itk.imread(input_path, pixel_type=itk.F)
+    os.makedirs(output_path, exist_ok=True)  # also holds intermediate results
 
     # segment the femoral and tibial cartilage
     obj = AnalysisObject()
@@ -57,8 +70,8 @@ def analysis_pipeline(input_path, output_path):
 
     # transform the thickness measurements to the atlas space
     # we use modelling transform, which is the inverse of the image resampling transform
-    transformed_mesh_FC = transform_mesh(distance_inner_FC, transform=phi_BA)
-    transformed_mesh_TC = transform_mesh(distance_inner_TC, transform=phi_BA)
+    transformed_mesh_FC = transform_mesh(distance_inner_FC, transform=phi_BA, filename_prefix=output_path + "/FC")
+    transformed_mesh_TC = transform_mesh(distance_inner_TC, transform=phi_BA, filename_prefix=output_path + "/TC")
 
     # Get inner and outer meshes for the TC and FC atlas meshes
     prob_fc_atlas = itk.imread(DATA_DIR / "atlases/atlas_60_LEFT_baseline_NMI/atlas_fc.nii.gz")
@@ -72,8 +85,6 @@ def analysis_pipeline(input_path, output_path):
     mapped_mesh_fc = mp.map_attributes(transformed_mesh_FC, inner_mesh_fc_atlas)
     mapped_mesh_tc = mp.map_attributes(transformed_mesh_TC, inner_mesh_tc_atlas)
 
-    os.makedirs(output_path, exist_ok=True)
-
     # Project thickness to 2D
     x, y, t = mp.project_thickness(mapped_mesh_tc, mesh_type='TC')
     s = plt.scatter(x, y, c=t, vmin=0, vmax=4)
@@ -82,7 +93,6 @@ def analysis_pipeline(input_path, output_path):
     plt.axis('off')
     plt.draw()
     plt.savefig(output_path + '/thickness_TC.png')
-    plt.show()
 
     x, y, t = mp.project_thickness(mapped_mesh_fc, mesh_type='FC')
     plt.figure(figsize=(8, 6))
@@ -92,7 +102,6 @@ def analysis_pipeline(input_path, output_path):
     plt.axis('off')
     plt.draw()
     plt.savefig(output_path + '/thickness_FC.png')
-    plt.show()
 
     out_image_path = os.path.join(output_path, "in_image.nrrd")
     itk.imwrite(in_image, out_image_path)  # for debugging
@@ -104,7 +113,7 @@ if __name__ == "__main__":
         r"M:\Dev\Osteoarthritis\NAS\OAIBaselineImages\results\0.C.2\9000798\20040924\10249506",
         r"M:\Dev\Osteoarthritis\NAS\OAIBaselineImages\results\0.C.2\9000798\20040924\10249512",
         r"M:\Dev\Osteoarthritis\NAS\OAIBaselineImages\results\0.C.2\9000798\20040924\10249516",
-        r"M:\Dev\Osteoarthritis\NAS\OAIBaselineImages\results\0.C.2\9000798\20040924\10249517",
+        # r"M:\Dev\Osteoarthritis\NAS\OAIBaselineImages\results\0.C.2\9000798\20040924\10249517",  # parsed incorrectly
     ]
 
     for i, case in enumerate(test_cases):
