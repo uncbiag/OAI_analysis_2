20c114132cf0976565d10547c8c68c56a808a5ef
Dženan Zukić <dzenan.zukic@kitware.com>
2024-09-10 11:56:56 -0400

    ENH: Pre-process the image intensities upon reading

--- oai_analysis/pipeline.py
+++ oai_analysis/pipeline.py
@@ -55,45 +55,44 @@ def analysis_pipeline(input_path, output_path, keep_intermediate_outputs):
     :param output_path: path to the desired directory for outputs.
     """
     in_image = itk.imread(input_path, pixel_type=itk.F)
+    in_image = preprocess(in_image, modality="mri")
     os.makedirs(output_path, exist_ok=True)  # also holds intermediate results
     if keep_intermediate_outputs:
         itk.imwrite(in_image, os.path.join(output_path, "in_image.nrrd"))
 
-    # segment the femoral and tibial cartilage
+    print("Segmenting the femoral and tibial cartilage")
     obj = AnalysisObject()
     FC_prob, TC_prob = obj.segment(in_image)
     if keep_intermediate_outputs:
-        itk.imwrite(FC_prob, os.path.join(output_path, "FC_prob.nrrd"), compression=True)
-        itk.imwrite(TC_prob, os.path.join(output_path, "TC_prob.nrrd"), compression=True)
+        print("Saving segmentation results")
+        itk.imwrite(FC_prob.astype(itk.F), os.path.join(output_path, "FC_prob.nrrd"), compression=True)
+        itk.imwrite(TC_prob.astype(itk.F), os.path.join(output_path, "TC_prob.nrrd"), compression=True)
 
-    # Get the thickness map for the meshes
+    print("Computing the thickness map for the meshes")
     distance_inner_FC, distance_outer_FC = mp.get_thickness_mesh(FC_prob, mesh_type='FC')
     distance_inner_TC, distance_outer_TC = mp.get_thickness_mesh(TC_prob, mesh_type='TC')
 
-    # Register the input image to the atlas
+    print("Registering the input image to the atlas")
     model = get_unigradicon()
     DATA_DIR = pathlib.Path(__file__).parent / "data"
     atlas_filename = DATA_DIR / "atlases/atlas_60_LEFT_baseline_NMI/atlas.nii.gz"
     in_image_D = in_image.astype(itk.D)
     atlas_image = itk.imread(atlas_filename, itk.D)
 
-    phi_AB, phi_BA = itk_wrapper.register_pair(model,
-                                               preprocess(in_image_D, modality="mri"),
-                                               preprocess(atlas_image, modality="mri"),
-                                               finetune_steps=None)  # each step takes several seconds
+    phi_AB, phi_BA = itk_wrapper.register_pair(model, in_image_D, atlas_image, finetune_steps=None)
     if keep_intermediate_outputs:
-        print("Saving registration results crashes, skipping")
-        # itk.transformwrite(phi_AB, os.path.join(output_path, "resampling.tfm"))
-        # itk.transformwrite(phi_BA, os.path.join(output_path, "modelling.tfm"))
+        print("Saving registration results")
+        itk.transformwrite(phi_AB, os.path.join(output_path, "resampling.tfm"))
+        itk.transformwrite(phi_BA, os.path.join(output_path, "modelling.tfm"))
 
-    # transform the thickness measurements to the atlas space
+    print("Transforming the thickness measurements to the atlas space")
     # we use modelling transform, which is the inverse of the image resampling transform
     transformed_mesh_FC = transform_mesh(distance_inner_FC, transform=phi_BA, filename_prefix=output_path + "/FC",
                                          keep_intermediate_outputs=keep_intermediate_outputs)
     transformed_mesh_TC = transform_mesh(distance_inner_TC, transform=phi_BA, filename_prefix=output_path + "/TC",
                                          keep_intermediate_outputs=keep_intermediate_outputs)
 
-    # Get inner and outer meshes for the TC and FC atlas meshes
+    print("Getting inner and outer meshes for the TC and FC atlas meshes")
     prob_fc_atlas = itk.imread(DATA_DIR / "atlases/atlas_60_LEFT_baseline_NMI/atlas_fc.nii.gz")
     mesh_fc_atlas = mp.get_mesh(prob_fc_atlas)
     inner_mesh_fc_atlas, outer_mesh_fc_atlas = mp.split_mesh(mesh_fc_atlas, mesh_type='FC')
@@ -103,11 +102,11 @@ def analysis_pipeline(input_path, output_path, keep_intermediate_outputs):
     inner_mesh_tc_atlas, outer_mesh_tc_atlas = mp.split_mesh(mesh_tc_atlas, mesh_type='TC')
     write_vtk_mesh(inner_mesh_tc_atlas, output_path + "/inner_mesh_tc_atlas.vtk")
 
-    # For mapping the thickness to the atlas mesh
+    print("Mapping the thickness to the atlas mesh")
     mapped_mesh_fc = mp.map_attributes(transformed_mesh_FC, inner_mesh_fc_atlas)
     mapped_mesh_tc = mp.map_attributes(transformed_mesh_TC, inner_mesh_tc_atlas)
 
-    # Project thickness to 2D
+    print("Projecting thickness to 2D")
     x, y, t = mp.project_thickness(mapped_mesh_tc, mesh_type='TC')
     s = plt.scatter(x, y, c=t, vmin=0, vmax=4)
     cb = plt.colorbar(s)
